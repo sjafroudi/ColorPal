eeb8ae0421465381e3f9b474b64c916c2ff995f5
merikurisalad <116539775+merikurisalad@users.noreply.github.com>
2023-07-05 03:04:01 -0700

    feat: install rne

--- colorpal/App.js
+++ colorpal/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cards from './Card';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>COLOR PAL!</Text>
+      {/* <Text>COLOR PAL!</Text> */}
+      <Cards></Cards>
       <StatusBar style="auto" />
     </View>
   );
