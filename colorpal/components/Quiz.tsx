e3ce411f25c9ea9e4c38b4ece3f5b51b4b59bc02
merikurisalad <116539775+merikurisalad@users.noreply.github.com>
2023-07-11 16:50:24 -0700

    feat: add quiz skeleton

--- colorpal/components/Quiz.tsx
+++ colorpal/components/Quiz.tsx
@@ -1,10 +1,110 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Modal } from "react-native";
+import data from "../data/QuizData";
+
+type Question = {
+  question: string;
+  options: string[];
+}
 
 function Quiz() {
+  const allQuestions: Question[] = data;
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentOptionSelected, setCurrentOptionSelected] = useState(null);
+  const [isOptionsDisabled, setIsOptionsDisabled] = useState(false);
+  const [showNextButton, setShowNextButton] = useState(false)
+  const [showResultModal, setShowResultModal] = useState(false)
+
+  const saveAnswer = (option: string) => {
+    if (option) {
+      // TODO: save selection option (only one) to array ... 
+      setShowNextButton(true)
+    }
+  }
+
+  // TODO: make next button look better
+  const renderNextButton = () => {
+    if (showNextButton) {
+      return (
+        <TouchableOpacity
+          onPress = {handleNext}
+          style={{
+            marginTop: 20, width: '100%', padding: 20, borderRadius: 5
+          }}>
+            <Text style={{fontSize: 20, textAlign: 'center'}}>Next</Text>
+        </TouchableOpacity>
+      )
+    } else {
+      return null;
+    }
+  }
+  
+  const handleNext = () => {
+    if (currentQuestionIndex == allQuestions.length-1) {
+      // Last Question
+      // TODO: Show result modal
+      setShowResultModal(true)
+    } else {
+      setCurrentQuestionIndex(currentQuestionIndex+1);
+      setCurrentOptionSelected(null);
+      setIsOptionsDisabled(false);
+      setShowNextButton(false);
+    }
+  }
+
+  const renderQuestion = () => {
+    return (
+      <View>
+        {/* Question Counter */}
+        <View style={{
+          flexDirection: 'row',
+          alignItems: 'flex-end'
+        }}>
+          <Text style={{fontSize: 20, marginRight: 2}}>{currentQuestionIndex+1}</Text>
+          <Text style={{fontSize: 18}}>/ {allQuestions.length}</Text>
+        </View>
+
+        {/* Question */}
+        <Text style={{
+          fontSize: 30
+        }}>{allQuestions[currentQuestionIndex]?.question}</Text>
+      </View>
+    )
+  }
+
+  const renderOptions = () => {
+    // TODO: keep selected option opaque
+    return (
+      <View>
+        {
+          allQuestions[currentQuestionIndex]?.options.map(option => (
+            <TouchableOpacity
+            onPress={() => saveAnswer(option)}
+            disabled={isOptionsDisabled}
+            key={option}
+            style={{
+              borderWidth: 3,
+              height: 60, borderRadius: 20,
+              flexDirection: 'row',
+              alignItems: 'center', justifyContent: 'space-between',
+              paddingHorizontal: 20,
+              marginVertical: 10
+            }}
+            >
+              <Text style={{fontSize: 20}}>{option}</Text>
+            </TouchableOpacity>
+          ))
+        }
+      </View>
+    )
+  }
+
+  // TODO: make spacing less awkward
   return (
     <View>
-      <Text>This is the quiz screen.</Text>  
+      {renderQuestion()}
+      {renderOptions()}
+      {renderNextButton()}
     </View>
   );
 }
---
+++ colorpal/data/QuizData.tsx
@@ -0,0 +1,22 @@
+export default data = [
+    {
+        question: "What color do you feel best represents your personality?",
+        options: ["Red", "Blue", "Green", "Yellow"]
+    },
+    {
+        question: "What color do you think best represents your creativity?",
+        options: ["Purple", "Orange", "Turquoise", "Pink"]
+    },
+    {
+        question: "What color do you think best represents your current mood?",
+        options: ["Black", "White", "Gray", "Brown"],
+    },
+    {
+        question: "What color do you think best represents your personality type?",
+        options: ["Bold and adventurous", "Serene and calm", "Friendly and sociable", "Focused and determined"]
+    },
+    {
+        question: "What color do you think represents your ideal self?",
+        options: ["Gold", "Silver", "Bronze", "Platinum"]
+    }
+]
