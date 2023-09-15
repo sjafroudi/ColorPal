ffeafdabdddbbf4546521353fa5cc6f5dae9282a
merikurisalad <116539775+merikurisalad@users.noreply.github.com>
2023-09-15 12:41:02 -0700

    feat: add logo and pic

--- colorpal/components/Home.tsx
+++ colorpal/components/Home.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import AwesomeButton from "./UploadPic";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 type HomeProps = {
   navigation: NativeStackNavigationProp<any>;
@@ -10,8 +10,13 @@ type HomeProps = {
 export default function Home({navigation}: HomeProps) {
     return (
         <View style={styles.container}>
+          <Image
+            source={require("../assets/logo.png")}
+            style={{width: 120, height: 60}} />
+          <Image
+            source={require("../assets/makeup-palette.png")}
+            style={{width: 120, height: 120}} /> 
           <AwesomeButton />
-          <Text>Welcome to Colorpal!</Text>
           <Button title="Take the Quiz" onPress={() => navigation.navigate('Quiz')} />
           <StatusBar style="auto" />
         </View>
